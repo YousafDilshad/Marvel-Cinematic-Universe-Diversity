8bf1d5a8d57b326dc3b1de2c89efd51f984b9142
YousafDilshad <157308072+YousafDilshad@users.noreply.github.com>
2024-05-08 18:46:06 -0400

    Updated with gender and race

--- data_generation.py
+++ data_generation.py
@@ -1,30 +1,46 @@
 import pandas as pd
 import openai
 
-# Set your API key
+#Setting API key
 api_key = "sk-proj-v6comklbJcwj7wX75eJfT3BlbkFJFUNpDGwR1ORsj1D5IKPY"
 openai.api_key = api_key
 
-# Load your dataframe
+#Loading data
 mcu_filtered = pd.read_csv("mcu_screentime_filtered.csv")
 
-# Define a function to generate gender for a given character name
+#Defining function to generate gender
 def get_gender(character_name):
     prompt = f"What is the gender of the character {character_name} in the Marvel Cinematic Universe? Provide a one word answer between Male and Female"
-    # Make a request to the OpenAI API using the specified model and prompt
+    #Sending a request to OpenAI API 
     response = openai.chat.completions.create(
             model="gpt-3.5-turbo-0125",
             messages=[{"role": "user", "content": prompt}]
         )
-    # Print the API response to console for debugging purposes
+    #Printing API esponse
     print(response)    
     
-    # Extract the summary from the API response and return it
+    #Extracting answer
     return response.choices[0].message.content
 
 
-# Apply the function to create a new column called 'gender'
+#Defining function to generate race
+def get_race(character_name):
+    prompt = f"What is the race of the character {character_name} in the Marvel Cinematic Universe? Provide a one/two word answer from the following list: Caucasian, Asian, African-American, Middle Eastern, Hispanic."
+    #Sending a request to OpenAI API 
+    response = openai.chat.completions.create(
+            model="gpt-3.5-turbo-0125",
+            messages=[{"role": "user", "content": prompt}]
+        )
+    #Printing API esponse
+    print(response)    
+    
+    #Extracting answer
+    return response.choices[0].message.content
+
+
+#Applying API response to create gender and race columns in dataframe
 mcu_filtered["gender"] = mcu_filtered["character_name"].apply(get_gender)
+mcu_filtered["race"] = mcu_filtered["character_name"].apply(get_race)
 
 # Save the updated dataframe
-mcu_filtered.to_csv("mcu_filtered_with_gender.csv", index=False)
+mcu_filtered.to_csv("mcu_filtered_with_gender_and_race.csv", index=False)
