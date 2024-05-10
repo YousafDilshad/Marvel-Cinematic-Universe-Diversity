73df44fcd726c0350b5875dcdced5fda1d6390f9
YousafDilshad <157308072+YousafDilshad@users.noreply.github.com>
2024-05-10 04:26:48 -0400

    4 Updated

--- data_visualization.py
+++ data_visualization.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 from matplotlib.patches import Patch
 
+
 # Importing data
 mcu_data = pd.read_csv("mcu_filtered_final.csv")
 
@@ -58,3 +59,54 @@ for i, bar in enumerate(bars_race):
 plt.tight_layout()
 plt.savefig('top_20_characters_race_specific_colors.png')  # Save the figure as an image file
 plt.show()
+
+#Importing box office and critical rating data
+mcu_boxoffice = pd.read_csv("mcu_box_office.csv")
+print(mcu_boxoffice)
+
+import pandas as pd
+
+# Assuming 'gender' and 'race' columns exist in mcu_data
+
+# Calculate total value of columns 1 to 27 by gender
+gender_totals = mcu_data.groupby('gender').sum().iloc[:, :27]
+
+# Calculate total value of columns 1 to 27 by race
+race_totals = mcu_data.groupby('race').sum().iloc[:, :27]
+
+# Calculate gender diversity for each column 1 to 27 where gender diversity = female/total
+gender_diversity = {}
+for column in mcu_data.columns[0:27]:
+    female_total = mcu_data[mcu_data['gender'] == 'Female'][column].sum()
+    total = mcu_data[column].sum()
+    gender_diversity[column] = 100*female_total / total
+
+# Calculate racial diversity for each column 1 to 27 where racial diversity = values that are not Caucasian/total
+racial_diversity = {}
+for column in mcu_data.columns[0:27]:
+    non_caucasian_total = mcu_data[mcu_data['race'] != 'Caucasian'][column].sum()
+    total = mcu_data[column].sum()
+    racial_diversity[column] = 100*non_caucasian_total / total
+
+print("Gender Totals:")
+print(gender_totals)
+print("\nRace Totals:")
+print(race_totals)
+print("\nGender Diversity:")
+print(gender_diversity)
+print("\nRacial Diversity:")
+print(racial_diversity)
+
+# Add gender diversity to mcu_boxoffice
+mcu_boxoffice['gender_diversity'] = mcu_boxoffice['movie_title'].map(gender_diversity)
+mcu_boxoffice['gender_diversity'] = mcu_boxoffice['gender_diversity'].round(2)
+
+# Add racial diversity to mcu_boxoffice
+mcu_boxoffice['racial_diversity'] = mcu_boxoffice['movie_title'].map(racial_diversity)
+mcu_boxoffice['racial_diversity'] = mcu_boxoffice['racial_diversity'].round(2)
+
+
+
+
+#Saving to file
+mcu_boxoffice.to_csv('mcu_boxoffice_with_diversity.csv', index=False)
